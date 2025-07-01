7671b6efa61c6c629bfc4f0d4c6d348bbc32af79
Chris Lalancette <clalancette@gmail.com>
2025-07-01 15:22:09 +0000

    Rebase from 'release/rolling/tracetools_launch'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'tracetools_launch'
 
 setup(
     name=package_name,
-    version='8.7.0',
+    version='8.8.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -15,6 +15,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
     ],
+    package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     maintainer=(
         'Christophe Bedard, '
