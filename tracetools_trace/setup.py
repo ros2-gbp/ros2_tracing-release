f6a759187987501505341bde66bd5c74c08ac614
Chris Lalancette <clalancette@gmail.com>
2025-07-29 20:48:24 +0000

    Rebase from 'upstream'

--- tracetools_trace/setup.py
+++ tracetools_trace/setup.py
@@ -5,7 +5,7 @@ package_name = 'tracetools_trace'
 
 setup(
     name=package_name,
-    version='8.8.0',
+    version='8.8.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -37,5 +37,9 @@ setup(
         ],
     },
     license='Apache 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
 )
